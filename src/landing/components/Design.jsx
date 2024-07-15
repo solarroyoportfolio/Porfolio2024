dd634d49a0ec0d963510f0aafd9ca8b2864969d4
María Sol Arroyo <mariasolarroyoc.web@gmail.com>
2024-07-14 21:09:29 -0300

    portfolio v1.7

--- src/landing/components/Design.jsx
+++ src/landing/components/Design.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Modal from "react-modal";
 
 import { SectionTitles } from "../../components/SectionTitles";
--- src/landing/components/Nav.jsx
+++ src/landing/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { HomeIcon } from "../../components/HomeIcon";
 import { UserIcon } from "../../components/UserIcon";
 import { SkillsIcon } from "../../components/SkillsIcon";
