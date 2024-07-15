1039f3b1e3f1e59b2073a945be3627f4bd5af143
María Sol Arroyo <mariasolarroyoc.web@gmail.com>
2024-07-14 21:09:09 -0300

    portfolio v1.7

--- src/landing/components/Nav.jsx
+++ src/landing/components/Nav.jsx
@@ -10,10 +10,10 @@ export const Nav = () => {
   const [active, setActive] = useState("#");
 
   return (
-    <nav className="text-color_gray inline-flex  bg-[#0000004d] w-auto py-4 px-8 z-20 fixed bottom-8 justify-center items-center transform -translate-x-[50%] left-1/2 gap-6 rounded-[3rem] backdrop-blur-sm">
+    <nav className="text-color_gray inline-flex  bg-[#0000004d] w-auto py-4 px-8 z-20 fixed bottom-8 justify-center items-center transform -translate-x-[50%] left-1/2 lg:gap-6 gap-2  rounded-[3rem] backdrop-blur-sm">
       <a
         onClick={() => setActive("#")}
-        className={`  p-3 rounded-[3rem]  text-[1rem] ${active === "#" ? "active bg-color_primary text-color_bg" : "hover:bg-[#0000004d] hover:text-color_white"}
+        className={`p-3 rounded-[3rem]  text-[1rem] ${active === "#" ? "active bg-color_primary text-color_bg" : "hover:bg-[#0000004d] hover:text-color_white"}
         `}
         href="#"
       >
